5714a7036125d11ec3acef06516039d4b9f8a49a
Morvint <sehavtsov@bk.ru>
2023-12-03 02:55:26 +0300

    Functions correct work blok

--- Main.py
+++ Main.py
@@ -3,6 +3,20 @@ import cv2
 # from PIL import Image
 # import matplotlib.pyplot as plt
 
+def foo(file_path):
+    file_path = "img.png"
+    img = cv2.imread(file_path)
+    size = img.shape
+    if(size[0] * size[1] % 8 != 0):
+        a = 8 - (size[0] % 8), 8 - (size[1] % 8)
+        if a[0] < a[1]:
+            dop = np.zeros((a[0], size[1], 3))
+            img = np.concatenate((img, dop), axis=0)
+        else:
+            dop = np.zeros((size[0], a[1], 3))
+            img = np.concatenate((img, dop), axis=1)
+    cv2.imwrite(file_path, img)
+
 def rev(var):
     """Функция инвертирования строки
     Передается строка"""
@@ -339,6 +353,7 @@ def main():
     file_name_newimg_copy = "newimg_copy.png"
 
     # Окрытие картинки и преобразование в необходимый формат
+    foo(file_name_img)
     img = cv2.imread(file_name_img)
     img_shape = np.array(img.shape)
     img = img.reshape(img_shape[0] * img_shape[1] * img_shape[2])
@@ -415,7 +430,7 @@ def main():
     decrypt = rev(decrypt)
 
     # Отсечение дополнительных бит
-    img = decrypt[0:img_shape[0] * img_shape[1] * img_shape[2] * 8]
+    img = decrypt[0:crypt_shape[0] * crypt_shape[1] * crypt_shape[2] * 8]
 
     # Преобразование строки в форму подходящую для картинки
     z = []
@@ -424,7 +439,7 @@ def main():
         z.append(j)
     z = np.array(z)
     img = z
-    img = img.reshape(img_shape[0], img_shape[1], img_shape[2])
+    img = img.reshape(crypt_shape[0], crypt_shape[1], crypt_shape[2])
 
     # Сохраниение картинки
     cv2.imwrite(file_name_decryptimg, img)
--- test.py
+++ test.py
@@ -1,18 +1,27 @@
-def rev(var):
-    res = ''
-    for i in range (len(var) // 5):
-        # print(term[i*5:i*5+5])
-        res = var[i*5:i*5+5] + res
-    return res
+import cv2
+import numpy as np
+
+# def rev(var):
+#     res = ''
+#     for i in range (len(var) // 5):
+#         # print(term[i*5:i*5+5])
+#         res = var[i*5:i*5+5] + res
+#     return res
 
 
-term = "wasd1wasd2dsaw3dsaw4"
-term = rev(term)
-print(term)
+# term = "wasd1wasd2dsaw3dsaw4"
+# term = rev(term)
+# # print(term)
 
-# message = ''
-# for i in range (len(term) // 5):
-#     print(term[i*5:i*5+5])
-#     message = term[i*5:i*5+5] + message
-# print(message)
-
+file_name_img = "img.png"
+img = cv2.imread(file_name_img)
+size = img.shape
+if(size[0] * size[1] % 8 != 0):
+    a = 8 - (size[0] % 8), 8 - (size[1] % 8)
+    if a[0] < a[1]:
+        dop = np.zeros((a[0], size[1], 3))
+        img = np.concatenate((img, dop), axis=0)
+    else:
+        dop = np.zeros((size[0], a[1], 3))
+        img = np.concatenate((img, dop), axis=1)
+cv2.imwrite(file_name_img, img)
